eda29d1524b16d9dfe0aa9340f576c460e962ef8
MTG\mtg09 <mtg.software.dev@gmail.com>
2022-12-22 17:17:28 +0400

    feat: add auth with firebase

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,9 +1,27 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient, QueryClientConfig } from '@tanstack/react-query';
+import { CONSTS } from './consts';
 
-export let queryClient = new QueryClient();
+export let queryClient: QueryClient;
 
 export const getQueryClient = () => {
   return queryClient;
 };
 
-export const createQueryClient = () => (queryClient = new QueryClient());
+const testingConfig: QueryClientConfig = {
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    // ✅ no more errors on the console
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    error: () => {},
+  },
+};
+
+export const createQueryClient = () =>
+  (queryClient = new QueryClient(CONSTS.isTestEnv ? testingConfig : {}));
+createQueryClient();
--- src/utils/wrapper.tsx
+++ src/utils/wrapper.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createQueryClient } from './apiClient';
+import { UserContextProvider } from '@/contexts/userContext';
 
 export const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [queryClient] = useState(() => createQueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UserContextProvider>{children}</UserContextProvider>
+    </QueryClientProvider>
+  );
 };
